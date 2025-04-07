0e937c05904e225332ee3067e6084aabc2ed88d7
zadmin <zadmin@vm-lab95-11-439.asia-south1-a.c.niit-stackroute-2.internal>
2025-04-07 18:20:42 +0530

    secret

--- leave_backend/src/main/java/com/leave/config/WebConfig.java
+++ leave_backend/src/main/java/com/leave/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
   public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")   // This applies to all endpoints
-              .allowedOrigins("*") // Allow requests from this origin
+              .allowedOrigins("http://15.168.114.125:3000","http://localhost:3000") // Allow requests from this origin
               .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow common HTTP methods
               .allowedHeaders("Content-Type", "Authorization")  // Allow these headers
               .allowCredentials(true);  // Allow cookies/credentials if needed
